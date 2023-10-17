41d0d3790a406735a5001e75c4816ce5b9857ac6
Mateusz Chwast <chwastmat@gmail.com>
2023-10-17 21:05:29 +0200

    add motor module

---
+++ Core/Program/motor.c
@@ -0,0 +1,9 @@
+/*
+ * motor.c
+ *
+ *  Created on: Oct 17, 2023
+ *      Author: Mateusz Chwast
+ */
+
+#include "motor.h"
+#include "main.h"
---
+++ Core/Program/motor.h
@@ -0,0 +1,13 @@
+/*
+ * motor.h
+ *
+ *  Created on: Oct 17, 2023
+ *      Author: Mateusz Chwast
+ */
+
+#ifndef PROGRAM_MOTOR_H_
+#define PROGRAM_MOTOR_H_
+
+
+
+#endif /* PROGRAM_MOTOR_H_ */
