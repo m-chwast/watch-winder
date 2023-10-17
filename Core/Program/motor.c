0a966fb6d4911bea7344d29ad134da8b26a91d51
Mateusz Chwast <chwastmat@gmail.com>
2023-10-18 01:36:49 +0200

    set and reset pins in one function

--- Core/Program/motor.c
+++ Core/Program/motor.c
@@ -10,15 +10,16 @@
 #include "main.h"
 
 
-static void ResetMotorPins(uint32_t noResetMask) {
+static void MotorPinsWrite(uint32_t setMask) {
 	//if any assert fails, function needs modifications to work correctly
 	static_assert(MOTOR_OUT1_GPIO_Port == MOTOR_OUT2_GPIO_Port);
 	static_assert(MOTOR_OUT1_GPIO_Port == MOTOR_OUT3_GPIO_Port);
 	static_assert(MOTOR_OUT1_GPIO_Port == MOTOR_OUT4_GPIO_Port);
 
-	uint32_t pinMask = MOTOR_OUT1_Pin | MOTOR_OUT2_Pin | MOTOR_OUT3_Pin | MOTOR_OUT4_Pin;
-	pinMask &= ~(noResetMask);
-	HAL_GPIO_WritePin(MOTOR_OUT1_GPIO_Port, pinMask, GPIO_PIN_RESET);
+	uint32_t resetMask = MOTOR_OUT1_Pin | MOTOR_OUT2_Pin | MOTOR_OUT3_Pin | MOTOR_OUT4_Pin;
+	resetMask &= ~(setMask);
+	HAL_GPIO_WritePin(MOTOR_OUT1_GPIO_Port, resetMask, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(MOTOR_OUT1_GPIO_Port, setMask, GPIO_PIN_SET);
 }
 
 void Motor_Step(Motor_Dir dir) {
@@ -26,23 +27,19 @@ void Motor_Step(Motor_Dir dir) {
 
 	switch(state) {
 		case 0: {
-			ResetMotorPins(MOTOR_OUT1_Pin);
-			HAL_GPIO_WritePin(MOTOR_OUT1_GPIO_Port, MOTOR_OUT1_Pin, 1);
+			MotorPinsWrite(MOTOR_OUT1_Pin);
 			break;
 		}
 		case 1: {
-			ResetMotorPins(MOTOR_OUT2_Pin);
-			HAL_GPIO_WritePin(MOTOR_OUT2_GPIO_Port, MOTOR_OUT2_Pin, 1);
+			MotorPinsWrite(MOTOR_OUT2_Pin);
 			break;
 		}
 		case 2: {
-			ResetMotorPins(MOTOR_OUT3_Pin);
-			HAL_GPIO_WritePin(MOTOR_OUT3_GPIO_Port, MOTOR_OUT3_Pin, 1);
+			MotorPinsWrite(MOTOR_OUT3_Pin);
 			break;
 		}
 		case 3: {
-			ResetMotorPins(MOTOR_OUT4_Pin);
-			HAL_GPIO_WritePin(MOTOR_OUT4_GPIO_Port, MOTOR_OUT4_Pin, 1);
+			MotorPinsWrite(MOTOR_OUT4_Pin);
 			break;
 		}
 	}
