ea65f083fa79343c43b9819fcb292bf260b51c2f
Mateusz Chwast <chwastmat@gmail.com>
2023-10-17 20:40:49 +0200

    setup gpio pins for stepper motor

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -98,10 +98,7 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-	if(HAL_GetTick() % 2500 == 0) {
-		Console_LogLn("Ping!");
-		Console_LogValLn("Time: ", HAL_GetTick() / 1000);
-	}
+
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
@@ -206,14 +203,20 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(BOARD_LED_GPIO_Port, BOARD_LED_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, MOTOR_OUT1_Pin|MOTOR_OUT2_Pin|MOTOR_OUT4_Pin|BOARD_LED_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin : BOARD_LED_Pin */
-  GPIO_InitStruct.Pin = BOARD_LED_Pin;
+  /*Configure GPIO pins : MOTOR_OUT1_Pin MOTOR_OUT2_Pin MOTOR_OUT4_Pin BOARD_LED_Pin */
+  GPIO_InitStruct.Pin = MOTOR_OUT1_Pin|MOTOR_OUT2_Pin|MOTOR_OUT4_Pin|BOARD_LED_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(BOARD_LED_GPIO_Port, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : MOTOR_OUT3_Pin */
+  GPIO_InitStruct.Pin = MOTOR_OUT3_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(MOTOR_OUT3_GPIO_Port, &GPIO_InitStruct);
 
 /* USER CODE BEGIN MX_GPIO_Init_2 */
 /* USER CODE END MX_GPIO_Init_2 */
