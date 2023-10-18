8e36823fa51219fe7d8223867ac43699edacb791
Mateusz Chwast <chwastmat@gmail.com>
2023-10-18 17:59:35 +0200

    set test movement

--- Core/Program/program.c
+++ Core/Program/program.c
@@ -17,14 +17,13 @@ void Program_Init(void) {
 }
 
 void Program_Loop(void) {
-	static uint16_t cnt;
-	static Motor_Dir dir = MOTOR_DIR_CLOCKWISE;
-	cnt++;
-	if(cnt == 2048) {
-		cnt = 0;
-		dir = !dir;
+	if(Motor_IsRunning()) {
+		return;
 	}
 
-	HAL_Delay(2);
-	Motor_Step(dir);
+	static Motor_Dir dir;
+	dir = !dir;
+
+	Motor_SetMovement(3 * 360, dir);
+	HAL_Delay(2000);
 }
