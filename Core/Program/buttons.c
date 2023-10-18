de2b9458aa65d195dbec565820fe0258aa01bf25
Mateusz Chwast <chwastmat@gmail.com>
2023-10-19 00:09:05 +0200

    add buttons module with exti callback

---
+++ Core/Program/buttons.c
@@ -0,0 +1,14 @@
+/*
+ * buttons.c
+ *
+ *  Created on: Oct 18, 2023
+ *      Author: Mateusz Chwast
+ */
+
+#include "buttons.h"
+#include "main.h"
+
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
+
+}
---
+++ Core/Program/buttons.h
@@ -0,0 +1,13 @@
+/*
+ * buttons.h
+ *
+ *  Created on: Oct 18, 2023
+ *      Author: Mateusz Chwast
+ */
+
+#ifndef PROGRAM_BUTTONS_H_
+#define PROGRAM_BUTTONS_H_
+
+
+
+#endif /* PROGRAM_BUTTONS_H_ */
