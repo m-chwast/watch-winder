452ec793f24d8f60a06db3b4546e52f714f9af88
Mateusz Chwast <chwastmat@gmail.com>
2023-10-20 00:46:31 +0200

    implement idle watch winder state

--- Core/Program/watch_winder.c
+++ Core/Program/watch_winder.c
@@ -10,17 +10,38 @@
 #include "modes.h"
 
 
+typedef enum {
+	WW_STATE_IDLE,
+	WW_STATE_ROTATION_INIT,
+} WatchWinderState;
+
 static struct WatchWinder {
+	WatchWinderState state;
 	bool isRotationRequested;
 } watchWinder;
 
 
 void WatchWinder_Manage(void) {
+	/*
 	if(watchWinder.isRotationRequested && Motor_IsRunning() == false) {
 		watchWinder.isRotationRequested = false;
 		Motor_SetSpeed(Modes_GetRevolutionsPerHour());
 		uint32_t revolutions = MOTOR_REVOLUTIONS_TO_DEGREES(Modes_GetRevolutionsPerCycle());
 		Motor_SetMovement(revolutions, MOTOR_DIR_CLOCKWISE);
+	}
+	*/
+
+	switch(watchWinder.state) {
+		case WW_STATE_IDLE: {
+			if(watchWinder.isRotationRequested && !Motor_IsRunning()) {
+				watchWinder.isRotationRequested = false;
+				watchWinder.state = WW_STATE_ROTATION_INIT;
+			}
+			break;
+		}
+		case WW_STATE_ROTATION_INIT: {
+			break;
+		}
 	}
 }
 
