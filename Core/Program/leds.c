0274ba6d43ca618c38fe5367d3d4d44bf9d16594
Mateusz Chwast <chwastmat@gmail.com>
2023-10-19 14:11:57 +0200

    add basic typedefs for standard leds
    also change code formatter slightly

--- Core/Program/leds.c
+++ Core/Program/leds.c
@@ -6,6 +6,31 @@
  */
 
 
+#include "main.h"
+
+typedef enum {
+	LED_STATE_IDLE,
+	LED_STATE_INIT,
+} Led_State;
+
+typedef enum {
+	LED_TYPE_STD,
+} Led_Type;
+
+typedef struct {
+	GPIO_TypeDef* const gpio;
+	const uint16_t pin;
+	Led_State state;
+	Led_Type type;
+	uint32_t time;
+} Led;
+
+static void LedManage(Led* led);
+
 void Leds_Manage(void) {
+
+}
+
+static void LedManage(Led* led) {
 
 }
